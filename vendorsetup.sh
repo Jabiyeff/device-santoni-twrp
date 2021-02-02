ce2b7abe2f1cad41716be07774d1d209bd95c087
Jabiyeff <cebiyevanar@gmail.com>
2021-02-02 21:01:42 +0400

    Update vendorsetup.sh
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -39,32 +39,33 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
         export PLATFORM_VERSION="16.1.0"
    	export PLATFORM_SECURITY_PATCH="2099-12-31"
    	export TW_DEFAULT_LANGUAGE="en"
-   	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-   	export FOX_USE_BASH_SHELL=1
-   	export FOX_ASH_IS_BASH=1
-   	export FOX_USE_NANO_EDITOR=1
-	export FOX_USE_TAR_BINARY=1
-	export FOX_USE_ZIP_BINARY=1
-   	export FOX_REPLACE_BUSYBOX_PS=1
+   	export OF_DONT_PATCH_ENCRYPTED_DEVICE="1"
+   	export FOX_USE_BASH_SHELL="1"
+   	export FOX_ASH_IS_BASH="1"
+   	export FOX_USE_NANO_EDITOR="1"
+	export FOX_USE_TAR_BINARY="1"
+	export FOX_USE_ZIP_BINARY="1"
+   	export FOX_REPLACE_BUSYBOX_PS="1"
    	# export OF_DISABLE_DM_VERITY_FORCED_ENCRYPTION="1"; # disabling dm-verity causes stability issues with some kernel 4.9 ROMs; but is needed for MIUI
 	# export OF_DISABLE_FORCED_ENCRYPTION=1
-   	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+   	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
+   	export OF_CHECK_OVERWRITE_ATTEMPTS="1"
    	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES="1"
    	export OF_USE_MAGISKBOOT="1"
-    export OF_USE_LOCKSCREEN_BUTTON=1
+    export OF_USE_LOCKSCREEN_BUTTON="1"
 	export OF_FORCE_MAGISKBOOT_BOOT_PATCH_MIUI="1"
    	export OF_NO_MIUI_OTA_VENDOR_BACKUP="1"
    	export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
     export OF_MAINTAINER=Jabiyeff
 
 	# OTA for custom ROMs
-        export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
-        export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+        export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES="1"
+        export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR="1"
 
         # -- add settings for R11 --
-        export FOX_R11=1
-        export OF_USE_TWRP_SAR_DETECT=1
-        export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+        export FOX_R11="1"
+        export OF_USE_TWRP_SAR_DETECT="1"
+        export OF_DISABLE_MIUI_OTA_BY_DEFAULT="1"
         export OF_QUICK_BACKUP_LIST="/system_root;/vendor;/data;/persist;/boot;"
         # -- end R11 settings --
 
