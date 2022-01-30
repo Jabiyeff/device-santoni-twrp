de1001cf3e2e2a778391bd87f7637dd0a7fade55
Jabiyeff <cebiyevanar@gmail.com>
2022-01-30 15:15:43 +0400

    Use correct vendor partition
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -46,6 +46,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export FOX_USE_XZ_UTILS="1"
    	export FOX_REPLACE_BUSYBOX_PS="1"
    	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
+   	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/bootdevice/by-name/cust"
    	export OF_CHECK_OVERWRITE_ATTEMPTS="1"
    	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES="1"
    	export OF_USE_MAGISKBOOT="1"
@@ -57,7 +58,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
    	export OF_MAINTAINER_AVATAR="/home/jabiyeff/ofrp/device/xiaomi/santoni/addon/avatar.png"
    	export LC_ALL="C"
    	export ALLOW_MISSING_DEPENDENCIES=true
-    export OF_SKIP_DECRYPTED_ADOPTED_STORAGE=true
+	export OF_SKIP_DECRYPTED_ADOPTED_STORAGE=true
 
    	# export OF_DISABLE_DM_VERITY_FORCED_ENCRYPTION="1"; # disabling dm-verity causes stability issues with some kernel 4.9 ROMs; but is needed for MIUI
    	export OF_FORCE_DISABLE_DM_VERITY_MIUI="1"
